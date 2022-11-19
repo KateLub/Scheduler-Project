35b6d94f6afb4d45f94ecf1cb260affb2a875500
Kate Lubenkova <katherine2014lub@gmail.com>
2022-11-18 19:17:08 -0800

    Fixed comments and added several features for Phase 3

--- README.md
+++ README.md
@@ -43,5 +43,23 @@ without making a new event
 * As a user, I want to be able to load all my events from file
 * As a user, I want to get the option of saving or not the recent changes(since the last downloaded version) whenever 
 I quit the application
-* As a user, I want to get the option of opening the last downloaded version from file, 
+* As a user, I want to get the option of opening the last downloaded version from file,
 whenever I start the application
+
+
+## Instructions for Grader for Phase 3
+
+- You can generate the first required event related to adding Xs to a Y by clicking "ok" to the Welcome
+popup that contains instructors for the user (will be the first thing to appear), then selecting "no" when 
+the application asks if you want to load previous events. Finally, it will ask you to name your list (can be anything), 
+after which you may input the name, due date, and (optionally) a description for the event and 
+click "make event", your event will then show up on the screen.
+- You can generate the second required event related to adding Xs to a Y by inputting the name, due-date, and 
+description for an event in the bottom panel of the application and clicking "make event"
+- My visual component is located in the message pane that pops up (titled "Welcome to Mnemosyne!")
+when the application is run
+- You can save the state of my application by closing the application through the button in the top left corner,
+and clicking "yes" when it asks whether you would like to save your events
+- You can reload the state of my application by selecting "yes" when you first open the 
+application and it asks if you want to load your events (this option will only appear if there are
+any events saved in myFile, otherwise, it will lead user straight to making a new list)
--- src/main/ui/gui/MnemosyneApp.java
+++ src/main/ui/gui/MnemosyneApp.java
@@ -8,21 +8,21 @@ import persistance.JsonWriteToFile;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+
+// represents the Mnemosyne app, setting up the GUI, and handling all the app's manipulations and interactions
+// with the EventList and Event class
 public class MnemosyneApp {
 
     private static final String JSON_STORE = "./data/myFile.json";
     private JsonWriteToFile jsonWriter;
     private JsonReadFromFile jsonReader;
 
-    private String[] columns;
-    private JLabel lblHeading;
+    private JLabel tableListNameHeading;
     private DefaultTableModel model;
     private JTable table;
 
@@ -36,33 +36,33 @@ public class MnemosyneApp {
     private JFrame frame;
     private JPopupMenu popupMenu;
 
+
     //MODIFIES: this
     //EFFECTS: runs Mnemosyne application
     public MnemosyneApp() {
+        jsonWriter = new JsonWriteToFile(JSON_STORE);
+        jsonReader = new JsonReadFromFile(JSON_STORE);
+        appPopUpIntroInstr();
         list = new EventList("New Event List");
 
-        lblHeading = new JLabel(list.getListName());
-        lblHeading.setHorizontalAlignment(JLabel.CENTER);
-        lblHeading.setFont(new Font("Times New Roman", Font.PLAIN, 24));
-
         setUpFrame();
-
         setUpEventTable();
-
         setUpMakeEventPanel();
-
         appEventListGuiSetUp();
+
     }
 
+    //MODIFIES: this
+    //EFFECTS: all GUI features responsible for making the main frame of the app
     public void setUpFrame() {
         frame = new JFrame("Mnemosyne App");
         frame.getContentPane().setLayout(new BorderLayout());
-        frame.getContentPane().add(lblHeading, BorderLayout.PAGE_START);
+
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-                int option = JOptionPane.showConfirmDialog(frame, "Would you lke to save your work?");
+                int option = JOptionPane.showConfirmDialog(frame, "Would you like to save your work?");
                 if (option == JOptionPane.YES_OPTION) {
                     saveEventList();
                     System.exit(0);
@@ -71,90 +71,117 @@ public class MnemosyneApp {
                 }
             }
         });
-        frame.setSize(550, 200);
+        frame.setSize(1000, 600);
         frame.setVisible(true);
     }
 
+    //EFFECTS: creates visual for Table List-Name Heading
+    private void setUpEventTableHeading() {
+        tableListNameHeading = new JLabel(list.getListName());
+        tableListNameHeading.setHorizontalAlignment(JLabel.CENTER);
+        tableListNameHeading.setFont(new Font("Times New Roman", Font.PLAIN, 24));
+
+        frame.getContentPane().add(tableListNameHeading, BorderLayout.PAGE_START);
+    }
+
+    //MODIFIES: this
+    //EFFECTS: all GUI features responsible for table representing Event List
     public void setUpEventTable() {
-        columns = new String[]{"Event Name", "Due Date", "Description", "Completed"};
+        setUpEventTableHeading();
+
+        String[] columns = new String[]{"Event Name", "Due Date", "Description", "Completed"};
         model = new DefaultTableModel();
-        table = new JTable(model);
         model.setColumnIdentifiers(columns);
+        table = new JTable(model);
+
+        table.getTableHeader().setFont(new Font("Courier", Font.BOLD, 15));
+        table.setFont(new Font("Times", Font.PLAIN, 12));
+        table.setSelectionBackground(new Color(72,105,134));
+
         JScrollPane scrollPane = new JScrollPane(table);
-        table.setFillsViewportHeight(true);
+        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
 
         popupMenu = new JPopupMenu();
         deleteEvent();
         completeEvent();
         changeEvent();
         table.setComponentPopupMenu(popupMenu);
-        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
     }
 
+    //MODIFIES: this
+    //EFFECTS: entire bottom panel in app, all buttons/text fields for making a new Event
     public void setUpMakeEventPanel() {
         JPanel buttonPanel = new JPanel();
         eventName = new JTextField("Name");
         eventDate = new JTextField("yyyy-mm-dd");
         eventDesc = new JTextField("Optional Description");
         makeNewEventButton = new JButton("Make Event");
+
         makeNewEventButton.addActionListener(e -> addEvent(
                 new Event(eventName.getText(), eventDate.getText(), eventDesc.getText())));
+
         buttonPanel.add(eventName);
         buttonPanel.add(eventDate);
         buttonPanel.add(eventDesc);
         buttonPanel.add(makeNewEventButton);
         frame.getContentPane().add(buttonPanel, BorderLayout.AFTER_LAST_LINE);
-
-        jsonWriter = new JsonWriteToFile(JSON_STORE);
-        jsonReader = new JsonReadFromFile(JSON_STORE);
     }
 
     //MODIFIES: this
     //EFFECTS: prints welcome message and checks whether user wants to load previous work
-
     public void appEventListGuiSetUp() {
         loadEventList();
-        appPopUpIntroInstr();
         if (list.getListName().equals("New Event List") && list.getEvents().isEmpty()) {
             namingList();
         } else {
             if (decideIfUsePreviousWork() == JOptionPane.YES_OPTION) {
+                tableListNameHeading.setText(list.getListName());
                 setUpTableWithSavedEvents();
             } else {
                 namingList();
+                list = new EventList(list.getListName());
             }
         }
-        changeHeading(list.getListName());
     }
 
     //MODIFIES: this
-    //EFFECTS: completes naming of new Event List
+    //EFFECTS: changes list name and table header name to user input,
+    // and shows the user a message confirming the change
     public void namingList() {
         list.setListName(nameEventList());
+        tableListNameHeading.setText(list.getListName());
         JOptionPane.showMessageDialog(frame, "The name of your list is now " + list.getListName());
     }
 
-
-    public void changeHeading(String label) {
-        lblHeading.setText(label);
-    }
-
+    //MODIFIES: this
+    //EFFECTS: asks user for a name for the Event List
     public String nameEventList() {
         return JOptionPane.showInputDialog("What would you like to name this list?");
     }
 
+    //MODIFIES: this
+    //EFFECTS: asks user if should load Event List from file
     public int decideIfUsePreviousWork() {
-        return JOptionPane.showConfirmDialog(frame, "Would you lke to Load your previous work?");
+        return JOptionPane.showConfirmDialog(frame, "Would you like to Load your previous work?");
     }
 
+    //MODIFIES: this
+    //EFFECTS: provides pop-up message with app logo when starting app, w/t instr for how it works
     public void appPopUpIntroInstr() {
-        JOptionPane.showMessageDialog(frame, "Welcome to Mnemosyne!"
-                + "\n To make a new event for your list, enter the list details "
-                + "\n in the bottom panel and click \"make event\". To delete, change or complete "
-                + "\n an event, right-click on the event and choose the option you prefer. "
-                + "\n Then, follow the prompts to complete your desired action!");
+        ImageIcon logo = new ImageIcon("./data/MnemosyneLogo.png");
+        Image image = logo.getImage(); // transform it
+        Image newImg = image.getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH);
+        logo = new ImageIcon(newImg);
+
+        JOptionPane.showMessageDialog(frame, "To make a new event for your list, enter the list details "
+                + "\nin the bottom panel and click \"Make Event\". To Delete, Change, or Complete "
+                + "\nan event, right-click on the event in the table and choose the option you prefer. "
+                + "\nThen, follow the prompts to complete your desired action!", "Welcome to Mnemosyne!",
+                JOptionPane.OK_OPTION, logo);
     }
 
+    //MODIFIES: this
+    //EFFECTS: all events transferred from list into empty table, in the correct format
     public void setUpTableWithSavedEvents() {
         String complete;
 
@@ -168,24 +195,36 @@ public class MnemosyneApp {
         }
     }
 
+    //MODIFIES: this
+    //EFFECTS: when user makes an event, event is added to the list, and to the table in correct format
     public void addEvent(Event e) {
-        if (list.addEventToList(e)) {
+        if (list.getListSize() == 0) {
+            list.addEventToList(e);
+            model.addRow(new Object[]{e.getEventName(), e.getEventDueDate(), e.getEventDescription(), "no"});
+        } else if (list.addEventToList(e)) {
             list.sortEventList();
             model.insertRow(list.getEvents().indexOf(e),
                     new Object[]{e.getEventName(), e.getEventDueDate(), e.getEventDescription(), "no"});
         }
     }
 
+    //MODIFIES: this
+    //EFFECTS: "Delete" option added to pop-up for each row/event on table,
+    // when user deletes an event, event is removed from list and from the table,
+    // and change confirmed with message
     public void deleteEvent() {
         JMenuItem deleteItem = new JMenuItem("Delete");
         deleteItem.addActionListener(e -> {
             list.removeEventFromList((String) model.getValueAt(table.getSelectedRow(), 0));
             model.removeRow(table.getSelectedRow());
-            JOptionPane.showMessageDialog(frame, "Right-click performed on table and choose DELETE");
+            JOptionPane.showMessageDialog(frame, "You have chosen to DELETE this event");
         });
         popupMenu.add(deleteItem);
     }
 
+    //MODIFIES: this
+    //EFFECTS: "Change" option added to pop-up for each row/event on table,
+    // which creates another pop-up for user to specifically choose what part of Event they want to change
     public void changeEvent() {
         JMenuItem changeItem = new JMenuItem("Change");
         changeItem.addActionListener(e -> {
@@ -195,11 +234,16 @@ public class MnemosyneApp {
                     JOptionPane.DEFAULT_OPTION,
                     JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
-            changeEventDetails(options[returnValue]);
+            if (returnValue != -1) {
+                changeEventDetails(options[returnValue]);
+            }
         });
         popupMenu.add(changeItem);
     }
 
+    //MODIFIES: this
+    //EFFECTS: using user input from changeEvent(), a pop-up is produced asking user what they want
+    // to change that part of Event to, and that Event is changed accordingly in the list, and the table
     public void changeEventDetails(String eventDetail) {
         String input = JOptionPane.showInputDialog(frame,
                 "What would you like to change the " + eventDetail + " to?");
@@ -210,19 +254,28 @@ public class MnemosyneApp {
             model.setValueAt(input, table.getSelectedRow(), 0);
         } else if (eventDetail.equals("Date")) {
             tempEvent.changeEventDueDate(input);
-            model.setValueAt(input, table.getSelectedRow(), 1);
+            list.sortEventList();
+            String complete = (String) model.getValueAt(table.getSelectedRow(), 3);
+            model.removeRow(table.getSelectedRow());
+            model.insertRow(list.getEvents().indexOf(tempEvent),
+                    new Object[]{tempEvent.getEventName(),
+                            tempEvent.getEventDueDate(), tempEvent.getEventDescription(), complete});
         } else {
             tempEvent.changeEventDescription(input);
             model.setValueAt(input, table.getSelectedRow(), 2);
         }
     }
 
+    //MODIFIES: this
+    //EFFECTS: "Complete" option added to pop-up for each row/event on table,
+    // which will change the value in the "Completed" column of that row/event to "yes" from "no",
+    // and confirms that change with message
     public void completeEvent() {
         JMenuItem completeEvent = new JMenuItem("Complete");
         completeEvent.addActionListener(e -> {
             list.findEvent((String) model.getValueAt(table.getSelectedRow(), 0)).completeEvent();
             model.setValueAt("yes", table.getSelectedRow(), 3);
-            JOptionPane.showMessageDialog(frame, "Right-click performed on table and choose Complete");
+            JOptionPane.showMessageDialog(frame, "You have chosen to COMPLETE this event");
         });
         popupMenu.add(completeEvent);
     }
@@ -236,9 +289,11 @@ public class MnemosyneApp {
             jsonWriter.open();
             jsonWriter.write(list);
             jsonWriter.close();
-            System.out.println(list.getListName() + " and all its' events has been saved to " + JSON_STORE);
+            JOptionPane.showMessageDialog(frame,
+                    list.getListName() + " and all its' events has been saved to " + JSON_STORE);
         } catch (FileNotFoundException e) {
-            System.out.println("Unable to write to file: " + JSON_STORE);
+            JOptionPane.showMessageDialog(frame,
+                    "Unable to write to file: " + JSON_STORE, "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
@@ -249,7 +304,8 @@ public class MnemosyneApp {
             list = jsonReader.read();
             list.sortEventList();
         } catch (IOException e) {
-            System.out.println("Unable to read from file: " + JSON_STORE);
+            JOptionPane.showMessageDialog(frame,
+                    "Unable to read from file: " + JSON_STORE, "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
